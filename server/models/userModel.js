4899723e6c8c5bb6604513b75f73ef06af603618
George M <gsmaroun@gmail.com>
2022-11-28 18:56:11 -0500

    fix the  db

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,14 +3,14 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 require('dotenv').config();
 // ${process.env.PASSWORD}
-const MONGO_URI = 'mongodb+srv://webpackmystack:lipfish@cluster0.ivrgrpq.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://webpackmystack:lipfish@cluster0.mqfyalr.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(MONGO_URI, {
   // options for the connect method to parse the URI
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // sets the name of the DB that our collections are part of
-  dbName: 'wpmsdb'
+  dbName: 'w'
 })
   .then(() => console.log('Connected to Mongo DB.'))
   .catch(err => console.log(err));
@@ -21,13 +21,13 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  template: { type: mongoose.SchemaTypes.Mixed, required: false }
+  //template: { type: mongoose.SchemaTypes.Mixed, required: false }
 });
 
 
-userSchema.plugin(passportLocalMongoose);
+//userSchema.plugin(passportLocalMongoose);
 
-const Users = mongoose.model('Users', userSchema);
+const Users = mongoose.model('users', userSchema);
 
 //module.exports = mongoose.model('userData', usersSchema, 'userData');
 
